ac50faea40916bc22d99ef00a32a316f55526c3f
kise <kaiazmi1412@gmail.com>
2021-09-15 23:17:01 +0530

    Indexing & slicing

---
+++ Indexing_&_slicing.py
@@ -0,0 +1,13 @@
+lst = ['one', 'two', 'three', 'four', 'five']
+
+print(lst[0:3]) # Multiple Indexing.
+
+print(lst[2::]) # "::" Slicing items.
+
+print(lst[-5]) # backway Indexing.
+
+# b = True # Subscript doesn't work here! 
+# print(lst[2]) # indexing through items
+
+course = "Python 101"
+print(course[5]) # Indexing in String.
