4c8541c097e323ac7a15cf076205d32fea0c0ca1
kise <kaiazmi1412@gmail.com>
2021-09-15 20:54:30 +0530

    Tuples Data Type

---
+++ Tuples.py
@@ -0,0 +1,7 @@
+# IMMUTABLE VS MUTABLE
+# NOT CHANGABLE VS CHANGABLE
+
+foods = ('Pizza', 'Fish', 'Tomatoes',) # In modern Python, we dont use , in the end but it's a good practice.
+
+for food in foods:
+    print("The food is", food)
