500d508d841317c010e72443cff3775218d34cd7
kise <kaiazmi1412@gmail.com>
2021-09-15 23:30:10 +0530

    TypeCasting

---
+++ Type_casting.py
@@ -0,0 +1,22 @@
+# age = input("What is your age? ")
+# data_type = type(age)
+# print(data_type) 
+# # It a String
+
+# age = int(age)
+# data_type = type(age)
+# print(data_type) 
+# # It a Integer
+
+# print("Your age in dog years is", age * 7)
+
+# grocery_list = ['Apples', 'Oranges', 'Bananas', 'Apples']
+# grocery_list = set(grocery_list)
+# print(grocery_list)
+# print(type(grocery_list))
+
+
+grocery_list = ['Apples', 'Oranges', 'Bananas', 'Apples']
+grocery_list = tuple(grocery_list)
+print(grocery_list)
+print(type(grocery_list))
