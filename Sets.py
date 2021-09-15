4918da9451c7273b2d7f288eaa675ecf7d4b266e
kise <kaiazmi1412@gmail.com>
2021-09-15 21:01:32 +0530

    Set Data type

---
+++ Sets.py
@@ -0,0 +1,10 @@
+# lst = [varname, varname2, varname3]
+# dictionary = {
+#     "key": "value",
+# }  # Examples.
+
+s = {"Item 1", "Item 2", "Item 3"}
+print(s) #1
+
+s = {"Item 1", "Item 2", "Item 2", "Item 3"}
+print(s) #2 Sets are unique. Inorder.
