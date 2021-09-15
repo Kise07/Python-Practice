cdda174be09da34d2c21016eb1281c6a5444b55e
kise <kaiazmi1412@gmail.com>
2021-09-16 00:12:31 +0530

    Compariosion Operators

---
+++ Comparision_operators.py
@@ -0,0 +1,33 @@
+# Example 1
+# can_code = True # or False
+
+# if can_code == True:
+#     # do a thing.
+#     print("You can code!")
+# else:
+#     # do something else.
+#     print("You don't know how to code yet!")
+
+
+# Example 2
+# teacher = "Kalob Taulein"
+
+# if teacher == "Kalob Taulien":
+#     print("Show the teacher portal")
+# else:
+#     print("You are a student, Welcome to PYthon 101")    
+
+# # For typos in Input 
+# teacher = "Kalob Taulein"
+
+# if teacher.lower() == "kalob Taulien":
+#     print("Show the teacher portal")
+# else:
+#     print("You are a student, Welcome to PYthon 101")
+
+# Example 3
+name = input("What is you name? ")
+if name == "Bob":
+    print("Welcome Bob!")
+else:
+    print("You're not Bob,  get outta here!")
