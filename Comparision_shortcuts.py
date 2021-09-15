bd3fa92dc30edc2170a229859623572f6b3ec240
kise <kaiazmi1412@gmail.com>
2021-09-16 00:46:49 +0530

    Comparision Shortcuts

---
+++ Comparision_shortcuts.py
@@ -0,0 +1,13 @@
+something = None # None will always return False
+
+if something:
+    print("This is true")
+else:
+    print("This is false")    
+
+something = "" # String will always return True
+
+if something:
+    print("This is true")
+else:
+    print("This is false")        
