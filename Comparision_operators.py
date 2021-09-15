2d9d1fe857749f745beeb5347933c4fdbf614d84
kise <kaiazmi1412@gmail.com>
2021-09-16 00:34:34 +0530

    Comparision Operator

--- Comparision_operators.py
+++ Comparision_operators.py
@@ -26,8 +26,41 @@
 #     print("You are a student, Welcome to PYthon 101")
 
 # Example 3
-name = input("What is you name? ")
-if name == "Bob":
-    print("Welcome Bob!")
+# name = input("What is you name? ")
+# if name == "Bob":
+#     print("Welcome Bob!")
+#     bring_food = "Pizza"
+# elif name == "Kalob"    :
+#     print("Welcome to your teacher portal")
+#     bring_food = "Tacos"
+# elif name == "Nathan"    :
+#     print("Welcome to the Gym")
+#     bring_food = "Weight protein"    
+# else:
+#     print("You're not Bob,  get outta here!")
+#     bring_food = "Salmon"
+
+# print(f"You sre eating {bring_food}")
+
+# # Example 4
+# name = input("What is you name? ")
+# name = name.lower()
+
+# if name != "Bob":
+#     print("You are not Bob, get outta here!")
+# else:
+#     print("Welcome Bobby boy")
+
+# Example 5
+age = 18
+if age <= 18:
+    print("You can Vote!")
 else:
-    print("You're not Bob,  get outta here!")
+    print("You can't Vote!")    
+
+# >
+# >=
+# <
+# <=
+# == is the same 
+# != is not the same   
