ed9cc14bd02a96c58d622cddc01ef9da77c23fdd
kise <kaiazmi1412@gmail.com>
2021-09-15 20:47:18 +0530

    Dictionary Data type

---
+++ Dictionary.py
@@ -0,0 +1,15 @@
+person = {
+    "key": "value",
+    "name": "Kise",
+    "age": 21
+}
+
+print(person["age"])
+
+print(person)
+
+person['status'] = "tainee" #Adding new item
+print(person)
+
+del person['key'] #Deleting item
+print(person)
