32f1c28dbfa54dcc42fa0e9fec76dd9c6a7854ae
kise <kaiazmi1412@gmail.com>
2021-09-16 17:17:41 +0530

    Multiple comparisiion operator

---
+++ Multiple_comparision_operators.py
@@ -0,0 +1,25 @@
+# method # 1 Simple Comparision Operator
+# age = 31
+# name= "Kalob"
+
+# if age >= 18:
+#     if name == "Kalob":
+#         print("I can drink wine")
+
+# method # 2 AND Comparision Operator (EveryOne needs to be true)
+age = 31
+name= "Kalob"
+
+if age >= 18 and name == "Kalob":
+    print("I can drink wine")
+else:
+    print("DONT DO ANYTHING")
+
+# method # 3 OR Comparision Operator (AnyOne needs to be true)
+age = 31
+name= "Jacob"
+
+if age >= 18 or name == "Kalob":
+    print("I can drink wine")
+else:
+    print("DONT DO ANYTHING")    
