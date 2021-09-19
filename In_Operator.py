f7a4fb22c1612cd6be3ca22a3eb3d240691b7db5
kise <kaiazmi1412@gmail.com>
2021-09-19 18:29:19 +0530

    #3 example

--- In_Operator.py
+++ In_Operator.py
@@ -1,7 +1,16 @@
-my_answer = input("What is your answer? ")
-options = ["rock", "paper", "scissors"]
+# my_answer = input("What is your answer? ")     #List
+# options = ["rock", "paper", "scissors"]
 
-if my_answer in options:
-    print("That option is a viable option!")
-else:
-    print("Wrong answer try again")    
+# if my_answer in options:
+#     print("That option is a viable option!")
+# else:
+#     print("Wrong answer try again")    
+
+key = "name"                   #Dictionary
+person = {
+    "name": "kalob",
+    "professional": "Coding teacher",
+}
+
+if key in person:
+    print("Name is a valid dictionary key in the person object")
