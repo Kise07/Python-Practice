1a0ab78958e705af5f51681d967d385091ef923f
kise <kaiazmi1412@gmail.com>
2021-09-19 17:51:42 +0530

    #2 example

--- In_Operator.py
+++ In_Operator.py
@@ -1,7 +1,7 @@
-my_answer = "rock"
+my_answer = input("What is your answer? ")
 options = ["rock", "paper", "scissors"]
 
 if my_answer in options:
-    print("Rock is one of the possible options!")
+    print("That option is a viable option!")
 else:
     print("Wrong answer try again")    
