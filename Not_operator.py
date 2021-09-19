baeeb5b53de81a55d2104aa06207845b45fffd7c
kise <kaiazmi1412@gmail.com>
2021-09-19 19:14:46 +0530

    Not Operator

---
+++ Not_operator.py
@@ -0,0 +1,9 @@
+# Comparing Values 
+my_thing = False          # Boolean
+
+if not my_thing:
+    print("Print a statement in here")
+    
+name = "Kane Ezki"        # List
+if name not in ['Kalob', 'Jon', 'Gully']:
+    print("Kane is not part of the club")
