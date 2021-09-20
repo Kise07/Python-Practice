a3e793e9b2adae913ae73696855fe05827ecbec8
kise <kaiazmi1412@gmail.com>
2021-09-20 17:42:02 +0530

    #2 looking for particular info in stacks

--- reading_multiple_lines.py
+++ reading_multiple_lines.py
@@ -1,2 +1,7 @@
-with open('emails.txt', 'r') as emails: #1 reading all the emails
-    print(emails.readlines())
+with open('emails.txt', 'r') as emails:  # looking for particular info in stack 
+    emails = emails.readlines()
+
+for email in emails:
+    print("Looking for a hotmails account")
+    if "hotmail" in email:
+        print(email)  
