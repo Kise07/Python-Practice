aa709bd454278454146b9d51eccc11814f3a081c
kise <kaiazmi1412@gmail.com>
2021-09-20 18:53:12 +0530

    Making a Simple Api request

---
+++ Simple_api_requests.py
@@ -0,0 +1,16 @@
+# Simple Request
+# import requests                      
+
+# req = requests.get("https://kalob.io")
+# print(req.status_code)
+
+# API Request
+import requests
+import time
+while True:
+    req = requests.get("https://courses.codingforeverybody.com")
+    print(req.status_code)
+    if req.status_code != 200:
+        # Email me or text me
+        pass
+    time.sleep(1)
