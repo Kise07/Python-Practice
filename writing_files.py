f7d1be71666ec15f4d3de2ba2f9c967f9a12efbc
kise <kaiazmi1412@gmail.com>
2021-09-20 17:32:11 +0530

    Two ways in writing & append

--- writing_files.py
+++ writing_files.py
@@ -1,3 +1,3 @@
-with open('writing_files.txt','a') as file:
+with open('writing_files.txt','a') as file: # a: append / w: write 
     file.write("\nA second line!")
     file.write("\n\tThis is tabbed!")
