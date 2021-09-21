7453351f61b38f051a7d8126ec20a5d35cbe3961
kise <kaiazmi1412@gmail.com>
2021-09-21 16:29:41 +0530

    Enumeration

---
+++ Enumeration.py
@@ -0,0 +1,6 @@
+animals = ['Gully', 'Rhubarb', 'Zephyr', 'Henry']
+for index, animal in enumerate(animals):     #Enumerate indexes values
+    # if index % 2 == 0:
+    #     continue
+    # print(animal)
+    print(f"{index+1}.\t{animal}")
