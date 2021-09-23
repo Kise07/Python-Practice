c4bb896d08904db358e44decad800cf0f075095f
kise <kaiazmi1412@gmail.com>
2021-09-23 20:05:19 +0530

    Class cleanup

---
+++ Class_cleanup.py
@@ -0,0 +1,14 @@
+class Animal:
+    fur_color = "Orange"
+
+    def speak(self):
+        print("Raawwwrr")
+
+    def eat(self):
+        pass
+
+    def chase(self):
+        pass
+
+tiger = Animal()
+tiger.speak()  
